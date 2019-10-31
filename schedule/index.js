8b9e5a3ae8e86c6aa78085e09aab102eec622552
Nick <nlemoing@edu.uwaterloo.ca>
2019-10-31 15:44:31 -0400

    working crud commands, minimal style

--- schedule/index.js
+++ schedule/index.js
@@ -11,20 +11,32 @@ class ScheduleModel {
         this._schedule = { name, semesters, courses, }
     }
 
-    addCourse({ subject, number, semester, }) {
-        this.courses.push({ subject, number, semester});
+    getCourse(id) {
+        return this._schedule.courses.find((course) => {
+            return course.id === id;
+        });
+    }
+
+    addCourse({ id, subject, number, semester, }) {
+        this._schedule.courses.push({ id, subject, number, semester});
+        return this.getCourse(id);
     }
 
     deleteCourse(id) {
-        if (id in this.courses) {
-            delete this.courses[id];
-        }
+        this._schedule.courses = this._schedule.courses.filter((course) => {
+            return course.id !== id;
+        });
+        return id;
     }
 
     editCourse(id, { semester, }) {
-        if (id in this.courses) {
-            this.courses[id].semester = semester;
-        }
+        this._schedule.courses = this._schedule.courses.map((course) => {
+            if (course.id === id) {
+                course.semester = semester;
+            }
+            return course;
+        });
+        return this.getCourse(id);
     }
 }
 
@@ -41,26 +53,61 @@ class ScheduleView {
         this.initialized = false;
     }
 
+    _addSemester(semester) {
+        const { id, name } = semester;
+        const semesterContainer = document.createElement('div');
+        semesterContainer.id = `semester-${id}`;
+        semesterContainer.classList.add('semester');
+        
+        const semesterHeader = document.createElement('h2');
+        semesterHeader.textContent = name;
+        semesterContainer.appendChild(semesterHeader);
+
+        const semestersContainer = document.getElementById('semesters');
+    
+        semestersContainer.appendChild(semesterContainer);
+    }
+
+    _addCourse(course) {
+        const { id, subject, number, semester, } = course;
+        const courseContainer = document.createElement('div');
+        courseContainer.id = `course-${id}`;
+        courseContainer.innerText = `${subject}${number}`;
+        courseContainer.classList.add('course');
+
+        const semesterContainer = 
+            document.getElementById(`semester-${semester}`) ||
+            document.getElementById('semester-misc');
+        semesterContainer.appendChild(courseContainer);
+    }
+
+    _deleteCourse(id) {
+        const courseToDelete = document.getElementById(`course-${id}`);
+        if (courseToDelete) {
+            courseToDelete.remove();
+        }
+    }
+
+    _editCourse(course) {
+        this._deleteCourse(course.id);
+        this._addCourse(course);
+    }
+
     // Render new schedule in the container
-    render({ name, semesters, }) {
+    render({ name, semesters, courses, }) {
         this._reset();
         
         const nameHeader = document.createElement('h1');
         nameHeader.textContent = name;
         this.container.appendChild(nameHeader);
 
-        for (const id in semesters) {
-            const { name, } = semesters[id];
-            const semesterContainer = document.createElement('div');
-            semesterContainer.id = `semester-{id}`;
-            semesterContainer.classList.add('semester');
-            
-            const semesterHeader = document.createElement('h2');
-            semesterHeader.textContent = name;
-            semesterContainer.appendChild(semesterHeader);
-        
-            this.container.appendChild(semesterContainer);
-        }
+        const semestersContainer = document.createElement('div');
+        semestersContainer.id = 'semesters';
+        this.container.appendChild(semestersContainer);
+
+        this._addSemester({ id: 'misc', name: 'Misc.', });
+        semesters.forEach(this._addSemester.bind(this));
+        courses.forEach(this._addCourse.bind(this));
 
         this.initialized = true;
     }
@@ -68,8 +115,14 @@ class ScheduleView {
     // When components are created, attach the relevant DOM listeners that dispatch eventBus events
 
     // Update components of the schedule
-    update() {
-
+    update(operation, data) {
+        if (operation === "addCourse") {
+            this._addCourse(data);
+        } else if (operation === "deleteCourse") {
+            this._deleteCourse(data);
+        } else if (operation === "editCourse") {
+            this._editCourse(data);
+        }
     }
 }
 
@@ -84,11 +137,27 @@ class ScheduleController {
 
     _bindEvents() {
         this._eventBus.on('createschedule', this.create.bind(this));
+        this._eventBus.on('addcourse', this.addCourse.bind(this));
     }
 
     create(schedule) {
         this.model.create(schedule);
         this.view.render(this.model.schedule);
+    }
+
+    addCourse(course) {
+        course = this.model.addCourse(course);
+        this.view.update("addCourse", course);
+    }
+
+    deleteCourse(id) {
+        id = this.model.deleteCourse(id);
+        this.view.update("deleteCourse", id);
+    }
+
+    editCourse(id, params) {
+        const course = this.model.editCourse(id, params);
+        this.view.update("editCourse", course);
     }
 }
 
@@ -124,13 +193,19 @@ const controller = new ScheduleController({ view, model, eventBus });
 controller.create({
     name: "My schedule",
     semesters: [
-        { name: "1A", },
-        { name: "1B", },
-        { name: "2A", },
-        { name: "2B", },
-        { name: "3A", },
-        { name: "3B", },
-        { name: "4A", },
-        { name: "4B", },    
+        { id: 1, name: "1A", },
+        { id: 2, name: "1B", },
+        { id: 3, name: "2A", },
+        { id: 4, name: "2B", },
+        { id: 5, name: "3A", },
+        { id: 6, name: "3B", },
+        { id: 7, name: "4A", },
+        { id: 8, name: "4B", },    
     ],
+    courses: [
+        { id: 1, subject: 'MATH', number: '135', semester: 1, },
+        { id: 2, subject: 'FUTURE', number: 'COURSE' }
+    ]
 });
+
+controller.addCourse({ id: 3, subject: 'ECON', number: '101' });
