cf5b44a4f2e51a5decebf38482146215817e894c
Nick Lemoing <nlemoing@munichre.com>
2019-11-04 19:40:47 +0000

    use API for courses

--- schedule/models/course.js
+++ schedule/models/course.js
@@ -1,25 +1,19 @@
 class CourseModel {
-    constructor() {
-        this._subjects = [
-            { id: 1, name: "ECON", },
-            { id: 2, name: "MATH", },
-        ]
-        this._courses = [
-            { id: 1, subject: 1, number: "101", longName: "Introduction to Microeconomics", },
-            { id: 2, subject: 1, number: "102", longName: "Introduction to Macroeconomics", },
-            { id: 3, subject: 2, number: "135", longName: "Algebra for Honours Mathematics"},
-            { id: 4, subject: 2, number: "137", longName: "Calculus 1 for Honours Mathematics"},
-        ]
+
+    async getSubjects() {
+        const subjects = await fetch('http://localhost:3000/subjects');
+        if (!subjects.ok) {
+            return [];
+        }
+        return await subjects.json();
     }
 
-    getSubjects() {
-        return this._subjects;
-    }
-
-    getNumbersForSubject(subjectId) {
-        return this._courses.filter((course) => {
-            return course.subject === subjectId;
-        });
+    async getNumbersForSubject(subjectId) {
+        const courses = await fetch(`http://localhost:3000/subjects/${subjectId}/courses`);
+        if (!courses.ok) {
+            return [];
+        }
+        return await courses.json();
     }
 }
 
