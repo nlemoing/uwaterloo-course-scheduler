374c80b7ee96ce80d8a45050c2271b38b26fca55
Nick <nlemoing@edu.uwaterloo.ca>
2019-11-06 11:24:13 -0500

    Complete course information scraper

--- scraper.py
+++ scraper.py
@@ -1,6 +1,113 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
+offered_regex = re.compile('Offered: ([FWS](,[FWS])*)')
+
+def check_ends(s, prefix='', suffix=''):
+    """
+    Helper function combining str.startswith and str.endswith
+    Returns True if s.startswith(prefix) and s.endswith(suffix) are True
+    Otherwise returns False
+    """
+    return s.startswith(prefix) and s.endswith(suffix)
+
+### Tests
+# assert check_ends('abc', prefix='a', suffix='c')
+# assert check_ends('abc', prefix='a')
+# assert check_ends('abc', suffix='c')
+# assert check_ends('abc')
+# assert not check_ends('abc', prefix='a', suffix='d')
+# assert not check_ends('abc', prefix='d', suffix='c')
+# assert not check_ends('abc', prefix='d', suffix='d')
+
+def remove_ends(s, prefix='', suffix=''):
+    """
+    Helper function that mixes startswith, endswith, and strip.
+    Takes a string and optional prefixes and suffixes.
+    Returns s with prefix and suffix removed, if they exist
+    """
+    if not s.startswith(prefix):
+        return remove_ends(s, suffix=suffix)
+    if not s.endswith(suffix):
+        return remove_ends(s, prefix=prefix)
+    return s[len(prefix) : len(s) - len(suffix)]
+
+### Tests
+# assert remove_ends('abc', prefix='a', suffix='c') == 'b'
+# assert remove_ends('abc', prefix='a') == 'bc'
+# assert remove_ends('abc', suffix='c') == 'ab'
+# assert remove_ends('abc') == 'abc'
+# assert remove_ends('abc', prefix='a', suffix='d') == 'bc'
+# assert remove_ends('abc', prefix='d', suffix='c') == 'ab'
+# assert remove_ends('abc', prefix='d', suffix='d') == 'abc'
+
+
+def parse_course(course_tree):
+    # course information is in a table and each component is in a tr tag
+    items = course_tree.find_all('tr')
+    if len(items) < 3:
+        return
+    course = {}
+    # The first three items are always the same
+    first, second, third, *rest = items
+    ## The first item should have two td components
+    first = [s for s in first.stripped_strings]
+    if len(first) != 2:
+        return
+    ## The first component has:
+    subject, number, course_type, num_credits = first[0].split(' ')
+    ### the course subject and number (e.g. MATH 135)
+    course['subject'] = subject
+    course['number'] = number
+    ### course type (combination of comma-separated LEC/LAB/TST/TUT)
+    course['type'] = course_type.split(',')
+    ### number of credits (e.g 0.50)
+    course['credits'] = float(num_credits)
+    ## The second component has a unique Waterloo course code (e.g. 006878)
+    course['code'] = remove_ends(first[1], prefix='Course ID: ')
+    ## The second item is just the course name (e.g. Algebra for Honours Mathematics)
+    course['name'] = second.string.strip()
+    ## The third item is the course description.
+    ### The description sometimes ends with [Offered: {terms}],
+    ### but sometimes that's in another item
+    course['description'] = third.string.strip()
+    offered_match = offered_regex.search(course['description'])
+    if offered_match and offered_match.group(1):
+        course['offered'] = offered_match.group(1).split(',')
+    ## The next items are variable, so we'll handle them in a loop
+    for item in rest:
+        # There may be some empty items, so skip over those
+        s = item.string.strip()
+        if not s:
+            continue
+        # Cases
+        ## Note: extra info. sometimes contains the [Offered: {terms}] pattern.
+        if check_ends(s, prefix='[Note: ', suffix=']'):
+            course['note'] = remove_ends(s, prefix='[Note: ', suffix=']')
+            offered_match = offered_regex.search(course['note'])
+            if offered_match and offered_match.group(1):
+                course['offered'] = offered_match.group(1).split(',')
+        ## Prereq, antireq, coreq: these describe dependencies on other courses
+        ## Remove their prefix and just store the string for now
+        ## More parsing will be done on these later
+        elif check_ends(s, prefix='Prereq: '):
+            course['prereq'] = remove_ends(s, prefix='Prereq: ')
+        elif check_ends(s, prefix='Antireq: '):
+            course['antireq'] = remove_ends(s, prefix='Antireq: ')
+        elif check_ends(s, prefix='Coreq: '):
+            course['coreq'] = remove_ends(s, prefix='Coreq: ')
+        ## Also/only offered online
+        elif 'online' in s.lower():
+            course['online'] = True
+        ## Instructor consent required
+        elif s.lower() == 'instructor consent required':
+            course['instructor_consent'] = True
+        elif 'other' in course:
+            course['other'].append(s)
+        else:
+            course['other'] = [s]
+    return course
 
 def scrape_subject(calendar, subject):
     url = "http://www.ucalendar.uwaterloo.ca/{}/COURSE/course-{}.html"
@@ -11,46 +118,8 @@ def scrape_subject(calendar, subject):
         return None
     soup = BeautifulSoup(page, 'html.parser')
     # each course is contained in a center tag
-    courses = soup.find_all('center')
-    for course in courses:
-        # course information is in a table and each component
-        # is in a tr tag
-        items = course.find_all('tr')
-        if len(items) < 3:
-            continue
-        # The first three items are always the same
-        first, second, third, *rest = items
-        ## The first item has:
-        ### the course code (e.g. MATH 135)
-        ### course type (combination of comma-separated LEC/LAB/TST/TUT)
-        ### number of credits (e.g 0.50)
-        ### Unique Waterloo course ID (e.g. 006878)
-        ## The first three components are contained in the same <td> tag,
-        ## while the course ID is in a separate <td> tag.
-        print([s for s in first.stripped_strings])
-        ## The second item is just the course name (e.g. Algebra for Honours Mathematics)
-        print(second.string)
-        ## The third item is the course description.
-        ### The description sometimes ends with [Offered: {terms}],
-        ### but sometimes that's in another item
-        print(third.string)
-        ## The next items are variable, so we'll handle them in a loop
-        for i, item in enumerate(rest):
-            # There may be some empty items, so skip over those
-            s = item.string.strip()
-            if not s:
-                continue
-            # Cases
-            ## Note: extra info. sometimes contains the [Offered: {terms}] pattern.
+    course_trees = soup.find_all('center')
+    courses = [parse_course(course_tree) for course_tree in course_trees]
+    return courses
 
-            ## Prereq, antireq, coreq: these describe dependencies on other courses
-            ## Remove their prefix and just store the string for now
-            ## More parsing will be done on these later
-
-            ## Also/only offered online
-
-            ## Instructor consent required
-            print(i, item.string.strip())
-
-
-scrape_subject("1920", "MATH")
+result = scrape_subject("1920", "MATH")
