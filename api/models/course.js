002b9799bbfd7beb7b1f4e0370c9d9f8bf05a3d4
Nick Lemoing <nlemoing@munichre.com>
2019-11-06 19:37:12 +0000

    use scraped results for courses

--- api/models/course.js
+++ api/models/course.js
@@ -1,15 +1,10 @@
+const courses = require('../../scraping/data/courses.json');
+const subjects = require('../../scraping/data/subjects.json');
+
 class CourseModel {
     constructor() {
-        this._subjects = [
-            { id: 1, name: "ECON", },
-            { id: 2, name: "MATH", },
-        ]
-        this._courses = [
-            { id: 1, subject: 1, number: "101", longName: "Introduction to Microeconomics", },
-            { id: 2, subject: 1, number: "102", longName: "Introduction to Macroeconomics", },
-            { id: 3, subject: 2, number: "135", longName: "Algebra for Honours Mathematics"},
-            { id: 4, subject: 2, number: "137", longName: "Calculus 1 for Honours Mathematics"},
-        ]
+        this._subjects = subjects;
+        this._courses = courses;
     }
 
     getCourse(courseId) {
@@ -24,7 +19,7 @@ class CourseModel {
 
     getCoursesForSubject(subjectId) {
         return this._courses.filter((course) => {
-            return course.subject === subjectId;
+            return course.subjectId === subjectId;
         });
     }
 }
